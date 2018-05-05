b47dcfea7ba468b4ebdb55f541a8de91c87e8d50
enigmaturing <javier.glez.martin@gmail.com>
2018-05-05 18:35:51 +0200

    Create an object of the class com.google.android.gms.maps.MapFragment. The object of the class MapFragment is necessary in order to be able to call its method getMapAsync as the next step to be able to modify the contents and the way the map is shown on the mapFragment (target, zoom, tilt, bearing..) and its objects (e.g. Markers...).

--- NoteMapActivity.java
+++ NoteMapActivity.java
@@ -3,6 +3,8 @@ package android.and06.einsendeaufgabe.mapfragment;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.MapFragment;
+
 public class NoteMapActivity extends Activity {
 
     @Override
@@ -11,5 +13,9 @@ public class NoteMapActivity extends Activity {
         // Set the content to the layout defined on activity_note_map.xml, that contains a MapFragment.
         // The map will be centered on the point where the Ecuador crosses the Prime Meridian.
         setContentView(R.layout.activity_note_map);
+        // Create an object of the class com.google.android.gms.maps.MapFragment. This object is
+        // necessary in order to be able to call its method getMapAsync as the next step to be able
+        // to modify the contents and the aspect of the map shown on the mapFragment.
+        MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.mapfragment);
     }
 }
